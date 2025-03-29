28d710a8dda46af9ea96d9f1c3088a296ab04984
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-03-30 00:09:39 +0200

    +

--- js/common.js
+++ js/common.js
@@ -5,9 +5,29 @@
 const headerCategoriesBtn = document.querySelector('[data-header-categories-btn]');
 const allCategories = document.querySelector('[data-all-categories]');
 const allCategoriesContainer = allCategories.querySelector('[data-all-categories-container]');
+const allCategoriesInner = allCategories.querySelector('[data-all-categories-inner]');
 
-headerCategoriesBtn.addEventListener('click', () => {
-   const rect = headerCategoriesBtn.getBoundingClientRect();
-   allCategoriesContainer.style.top = `${rect.bottom + window.scrollY}px`;
-   allCategories.classList.toggle('all-categories--active');
-});
+if (headerCategoriesBtn && allCategories && allCategoriesContainer && allCategoriesInner) {
+
+   // open
+   headerCategoriesBtn.addEventListener('click', () => {
+      const rect = headerCategoriesBtn.getBoundingClientRect();
+      allCategoriesContainer.style.top = `${rect.bottom + window.scrollY}px`;
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      allCategories.classList.toggle('all-categories--active');
+      document.querySelector('body').classList.toggle('no-scroll');
+   });
+
+   // close
+   allCategories.addEventListener('click', () => {
+      allCategories.classList.toggle('all-categories--active');
+      allCategoriesContainer.removeAttribute('style');
+      document.querySelector('body').classList.toggle('no-scroll');
+   });
+
+   allCategoriesInner.addEventListener('click', (e) => {
+      e.stopPropagation();
+   });
+} else {
+   console.error('error: ALL CATEGORIES');
+}
