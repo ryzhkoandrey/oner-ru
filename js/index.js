5be36ea7337b0fe99992dc9bf87eb1d2773ee6ff
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-03-30 02:26:46 +0200

    +

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// ----------  ----------
+// ---------- SLIDER ----------
+
+// swiper
+
+const slider = document.querySelector('[data-slider]');
+
+if (typeof Swiper !== 'undefined' && slider) {
+   const sliderSwiper = new Swiper(slider, {
+      slidesPerView: 1,
+      spaceBetween: 30,
+
+      navigation: {
+         prevEl: '[data-slider-prev]',
+         nextEl: '[data-slider-next]',
+      },
+
+      pagination: {
+         el: '[data-slider-pagination]',
+         clickable: true,
+      },
+   });
+} else {
+   console.error('error: slider');
+}
