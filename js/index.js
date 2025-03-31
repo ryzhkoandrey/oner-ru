d7bd8591414f2ce80e71aa56b2a7865ee0e0e44b
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-03-31 18:36:01 +0300

    +

--- js/index.js
+++ js/index.js
@@ -26,3 +26,24 @@ if (typeof Swiper !== 'undefined' && slider) {
 } else {
    console.error('error: slider');
 }
+
+// ---------- RECOMMENDED ----------
+
+// swiper
+
+const recommended = document.querySelector('[data-recommended]');
+
+if (typeof Swiper !== 'undefined' && recommended) {
+   const recommendedSwiper = new Swiper(recommended, {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      speed: 500,
+
+      navigation: {
+         prevEl: '[data-recommended-prev]',
+         nextEl: '[data-recommended-next]',
+      },
+   });
+} else {
+   console.error('error: recommended');
+}
