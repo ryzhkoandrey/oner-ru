37f5fd339f66e1cf0729d3fa8c696378eb0fde55
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-18 12:22:17 +0300

    +

--- js/catalog.js
+++ js/catalog.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// ----------  ---------- //
+// ---------- FILTER ---------- //
+
+const filterAccordions = document.querySelectorAll('[data-filter-accordion]');
+
+filterAccordions.forEach((accordion) => {
+   const accordionToggler = accordion.querySelector(
+      '[data-filter-accordion-toggler]'
+   );
+   const accordionBody = accordion.querySelector('[data-filter-accordion-body]');
+
+   if (accordionToggler && accordionBody) {
+      accordionToggler.addEventListener('click', () => {
+         const isOpen = accordion.classList.toggle('filter--active');
+
+         if (isOpen) {
+            accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
+         } else {
+            accordionBody.removeAttribute('style');
+         }
+      });
+   }
+});
