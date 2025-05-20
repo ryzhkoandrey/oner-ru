600e9496172631662bd3f8d9c934a596cc3d672e
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-20 11:47:29 +0300

    +

--- js/catalog.js
+++ js/catalog.js
@@ -2,45 +2,45 @@
 
 // ---------- FILTERS ---------- //
 
-// filters accordion
-
-const filtersAccordions = document.querySelectorAll('[data-filters-accordion]');
 const isFiltersTablet = () => window.matchMedia('(max-width: 1024px)').matches;
-
-filtersAccordions.forEach((accordion) => {
-   const accordionToggler = accordion.querySelector(
-      '[data-filters-accordion-toggler]'
-   );
-   const accordionBody = accordion.querySelector('[data-filters-accordion-body]');
-
-   if (accordionToggler && accordionBody) {
-      if (accordion.classList.contains('filters--active')) {
-         accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
-      }
-
-      accordionToggler.addEventListener('click', () => {
-         if (isFiltersTablet()) {
-            const isOpen = accordion.classList.toggle('filters--active');
-
-            if (isOpen) {
-               accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
-            } else {
-               accordionBody.removeAttribute('style');
-            }
-         }
-      });
-   } else {
-      console.error('error: filters accordion');
-   }
-});
-
-// filter accordion
-
-const filterAccordions = document.querySelectorAll('[data-filter-accordion]');
 
 const setAccordionHeight = (accordionBody) => {
    accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
 };
+
+// filters accordion
+
+const filtersAccordion = document.querySelector('[data-filters-accordion]');
+const filtersAccordionToggler = filtersAccordion.querySelector(
+   '[data-filters-accordion-toggler]'
+);
+const filtersAccordionBody = filtersAccordion.querySelector(
+   '[data-filters-accordion-body]'
+);
+
+if (filtersAccordion && filtersAccordionToggler && filtersAccordionBody) {
+   if (filtersAccordion.classList.contains('filters--active')) {
+      setAccordionHeight(filtersAccordionBody);
+   }
+
+   filtersAccordionToggler.addEventListener('click', () => {
+      if (isFiltersTablet()) {
+         const isOpen = filtersAccordion.classList.toggle('filters--active');
+
+         if (isOpen) {
+            setAccordionHeight(filtersAccordionBody);
+         } else {
+            filtersAccordionBody.removeAttribute('style');
+         }
+      }
+   });
+} else {
+   console.error('error: filters accordion');
+}
+
+// filter accordion
+
+const filterAccordions = document.querySelectorAll('[data-filter-accordion]');
 
 filterAccordions.forEach((accordion) => {
    const accordionToggler = accordion.querySelector(
