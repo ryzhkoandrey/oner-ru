406ebf565ab1172c405ebd20f9cc5e8f4c614248
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-05-22 17:13:00 +0300

    +

--- js/brands.js
+++ js/brands.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// ----------  ---------- //
+// ---------- BRANDS LETTER ---------- //
+
+const brandsLetters = document.querySelectorAll('[data-brands-letter]');
+
+if (brandsLetters.length > 0) {
+   brandsLetters.forEach((letter) => {
+      letter.addEventListener('click', () => {
+         brandsLetters.forEach((item) =>
+            item.classList.remove('brands__letter--active')
+         );
+         letter.classList.add('brands__letter--active');
+      });
+   });
+} else {
+   console.error('error: brandsLetters is empty');
+}
